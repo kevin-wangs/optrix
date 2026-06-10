b891260be3e4aca2542c5ef8f188f8fe78852ae8
kevin-wangs <kevin-wangs@users.noreply.github.com>
2026-06-10 09:44:50 +0200

    feat: wire up public API exports in __init__

--- src/optrix/__init__.py
+++ src/optrix/__init__.py
@@ -1,3 +1,13 @@
 """Optrix - High-performance parallel compute primitives."""
 __version__ = "0.1.0"
 __author__ = "Kevin Wangs"
+
+from optrix.core import detect_devices, synchronize
+from optrix.memory import zeros, empty, empty_like, DeviceBuffer
+from optrix.types import float16, float32, float64, int32, int64
+
+__all__ = [
+    "detect_devices", "synchronize",
+    "zeros", "empty", "empty_like", "DeviceBuffer",
+    "float16", "float32", "float64", "int32", "int64",
+]
