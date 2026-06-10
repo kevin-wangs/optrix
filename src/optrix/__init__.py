e0449c3599c2808142056eebd43ee8a287fdd6ec
kevin-wangs <kevin-wangs@users.noreply.github.com>
2026-06-10 09:49:04 +0200

    refactor: consolidate exports in __init__.py

--- src/optrix/__init__.py
+++ src/optrix/__init__.py
@@ -7,11 +7,19 @@ from optrix.memory import zeros, empty, empty_like, DeviceBuffer
 from optrix.types import float16, float32, float64, int32, int64
 from optrix.dispatch import dispatch, register_kernel, list_kernels, auto_grid
 from optrix.device import Device, get_device
+from optrix.stream import Stream, StreamPool
+from optrix.context import device_context, stream_context, current_context
+from optrix.pool import MemoryPool, get_pool
+from optrix.profiler import Profiler, profile
+from optrix.linalg import matmul, dot, norm, solve, svd
 
 __all__ = [
     "detect_devices", "synchronize",
     "zeros", "empty", "empty_like", "DeviceBuffer",
     "float16", "float32", "float64", "int32", "int64",
     "dispatch", "register_kernel", "list_kernels", "auto_grid",
-    "Device", "get_device",
+    "Device", "get_device", "Stream", "StreamPool",
+    "device_context", "stream_context", "current_context",
+    "MemoryPool", "get_pool", "Profiler", "profile",
+    "matmul", "dot", "norm", "solve", "svd",
 ]
