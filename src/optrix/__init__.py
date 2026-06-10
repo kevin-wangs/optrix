0991aafdd0a3b56efcffdce9cb4c8a9071447aea
kevin-wangs <kevin-wangs@users.noreply.github.com>
2026-06-10 09:44:55 +0200

    feat: export dispatch and device APIs

--- src/optrix/__init__.py
+++ src/optrix/__init__.py
@@ -5,9 +5,13 @@ __author__ = "Kevin Wangs"
 from optrix.core import detect_devices, synchronize
 from optrix.memory import zeros, empty, empty_like, DeviceBuffer
 from optrix.types import float16, float32, float64, int32, int64
+from optrix.dispatch import dispatch, register_kernel, list_kernels, auto_grid
+from optrix.device import Device, get_device
 
 __all__ = [
     "detect_devices", "synchronize",
     "zeros", "empty", "empty_like", "DeviceBuffer",
     "float16", "float32", "float64", "int32", "int64",
+    "dispatch", "register_kernel", "list_kernels", "auto_grid",
+    "Device", "get_device",
 ]
